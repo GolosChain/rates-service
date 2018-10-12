2d2b49cb19b8feb92b31378cb727602c3aaf54f8
Ilya Lebedev <bacherko@gmail.com>
2018-10-12 17:55:47 +0300

    fix

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,10 +1,8 @@
-function injectGolosGBGRate(quotes) {
-    quotes.GBG.GOLOS = quotes.GBG.USD / quotes.GOLOS.USD;
-    quotes.GOLOS.GBG = 1 / quotes.GBG.GOLOS;
+module.exports = class Utils {
+    static injectGolosGBGRate(quotes) {
+        quotes.GBG.GOLOS = quotes.GBG.USD / quotes.GOLOS.USD;
+        quotes.GOLOS.GBG = 1 / quotes.GBG.GOLOS;
 
-    return quotes;
-}
-
-module.exports = {
-    injectGolosGBGRate,
+        return quotes;
+    }
 };
