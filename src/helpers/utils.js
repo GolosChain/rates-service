3a0d5afacbe10a8b774662e71f1c63188ed05dd9
Ilya Lebedev <bacherko@gmail.com>
2018-10-12 16:58:50 +0300

    fix golos-gbg

---
+++ src/helpers/utils.js
@@ -0,0 +1,10 @@
+function injectGolosGBGRate(quotes) {
+    quotes.GBG.GOLOS = quotes.GBG.USD / quotes.GOLOS.USD;
+    quotes.GOLOS.GBG = 1 / quotes.GBG.GOLOS;
+
+    return quotes;
+}
+
+module.exports = {
+    injectGolosGBGRate,
+};
